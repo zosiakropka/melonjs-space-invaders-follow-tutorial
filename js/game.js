ace19ed069c53552867cc48cf412e1a7b09815b4
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2018-01-10 23:51:38 +0100

    Remove unnecessary stuff from the boilerplate

--- js/game.js
+++ js/game.js
@@ -27,11 +27,7 @@ var game = {
 
     // Run on game resources loaded.
     "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
         me.state.set(me.state.PLAY, new game.PlayScreen());
-
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
 
         // Start the game.
         me.state.change(me.state.PLAY);
