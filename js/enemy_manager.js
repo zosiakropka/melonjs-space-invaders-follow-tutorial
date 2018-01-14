98da320edd31b22458f6a13e48851c6aea0a5e33
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2018-01-14 14:23:17 +0100

    Let enemy go to right, a row down, to left, a row down, loop

--- js/enemy_manager.js
+++ js/enemy_manager.js
@@ -23,11 +23,32 @@ game.EnemyManager = me.Container.extend({
   onActivateEvent: function () {
     var self = this;
     this._timer = me.timer.setInterval(function () {
-        self.pos.x += self.vel;
-    }, 1000);
+        var bounds = self.childBounds;
+
+        if (
+          (self.vel > 0 && (bounds.right + self.vel) >= me.game.viewport.width) || // going right and would exceed right boundary if moved any more
+          (self.vel < 0 && (bounds.left + self.vel) <= 0) // going left and would exceed left boundary if moved any more
+        ) {
+          self.vel *= -1; // change direction to opposite
+          self.pos.y += 16; // go lower by 16 points
+
+          if (self.vel > 0) {
+            self.vel += 5; // speed up to the right at the upcoming row
+          } else {
+            self.vel -= 5; // speed up to the left at upcoming row
+          }
+        } else {
+          self.pos.x += self.vel; // move by `vel` points - either left or right
+        }
+    }, 1000); // each second
   },
 
   onDeactivateEvent: function () {
     me.timer.clearInterval(this._timer);
+  },
+
+  update: function (time) {
+    this._super(me.Container, 'update', [time]);
+    this.updateChildBounds();
   }
 })
